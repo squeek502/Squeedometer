65b9b68754d624920b212f3a864ca10771e6e1c3
squeek <squeek502@hotmail.com>
2016-06-14 23:03:10 -0700

    Switch back to capitalized mod id to keep it consistent between MC versions

--- java/squeek/speedometer/ModInfo.java
+++ java/squeek/speedometer/ModInfo.java
@@ -2,7 +2,7 @@ package squeek.speedometer;
 
 public final class ModInfo
 {
-	public static final String MODID = "squeedometer";
+	public static final String MODID = "Squeedometer";
 	public static final String VERSION = "${version}";
 	public static final String GUI_FACTORY_CLASS = "squeek.speedometer.gui.config.GuiFactory";
 }
